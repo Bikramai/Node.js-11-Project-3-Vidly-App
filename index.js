47bd9d5b6e4acb2817fb45911a32c07a2d92d5a6
bikramia <bikram.rhce@gmail.com>
2024-05-11 19:52:34 -0400

    Build the Rentals API

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
-const movies = require('./routes/movies')
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
@@ -10,9 +11,11 @@ mongoose.connect('mongodb://localhost/vidly')
     .catch(err => console.error('Could not connect to MongoDB...'))
 
 app.use(express.json());
-app.use('/api/genres', genres)
+app.use('/api/genres', genres);
 app.use('/api/customers', customers);
-app.use('/api/movies', movies)
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+
 
 
 // PORT
