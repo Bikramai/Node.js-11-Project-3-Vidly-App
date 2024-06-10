c34707cfa241350bf795fa9fafc766918ca15391
bikramia <bikram.rhce@gmail.com>
2024-06-09 21:55:23 -0400

    Using lodash package

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -10,15 +11,17 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     await user.save();
     
-    res.send(user);
+    //Pick or lodash method 
+    res.send(_.pick(user, ['_id', 'name', 'email']));
+    
+    // res.send({  // Manual method
+    //     user: user.name,
+    //     email: user.email
+    // });
 });
 
 // router.post('/', async (req, res) => {
