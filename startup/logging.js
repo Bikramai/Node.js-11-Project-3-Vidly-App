75a8ba3e0d0cc99ab9662fc46e7093eef693723d
bikramia <bikram.rhce@gmail.com>
2024-06-23 14:22:26 -0400

    Showing Unhandled Exceptions on the Console

--- startup/logging.js
+++ startup/logging.js
@@ -1,10 +1,12 @@
 const winston = require('winston');
+const { colorize } = require('winston/lib/winston/config');
 require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
     winston.handleExceptions(
-        new winston.transports.File({ filename: 'uncoghtExceptions.log'}))
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
+        new winston.transports.File({ filename: 'uncoghtExceptions.log'}));
     
     process.on('unhandleRejection', (ex) => {
         throw ex;
