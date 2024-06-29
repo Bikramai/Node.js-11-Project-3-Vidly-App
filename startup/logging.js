32e0e0a4b8f524060dcedb60a8c86b9981fca729
bikramia <bikram.rhce@gmail.com>
2024-06-29 11:41:19 -0400

    Preparing the app for Integration Testing

--- startup/logging.js
+++ startup/logging.js
@@ -1,6 +1,6 @@
 const winston = require('winston');
 const { colorize } = require('winston/lib/winston/config');
-require('winston-mongodb');
+// require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -12,11 +12,11 @@ module.exports = function() {
         throw ex;
     });
     
-    winston.add(winston.transports.File, { filename: 'logfile.log' });
-    winston.add(winston.transports.MongoDB, { 
-        db: 'mongodb://localhost/vidly',
-        level: 'info'
-     });
+    // winston.add(winston.transports.File, { filename: 'logfile.log' });
+    // winston.add(winston.transports.MongoDB, { 
+    //     db: 'mongodb://localhost/vidly',
+    //     level: 'info'
+    //  });
     
      const p = Promise.reject(new Error('Something failed misserably!'));
      p.then(() => console.log('Done'));
