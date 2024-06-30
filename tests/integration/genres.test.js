446688357d0e87491087e7fc8be0cc132e27e385
bikramia <bikram.rhce@gmail.com>
2024-06-30 07:50:44 -0400

    Populating the Test Db

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -5,18 +5,30 @@ let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); })
-    afterEach(() => { server.close(); });
+    afterEach(async () => { 
+        server.close(); 
+        await Genre.removeAllListeners({});
+    });
 
     describe('GET /', () => {
         it('should return all genres',  async () => {
             await Genre.collection.insertMany([
                 { name: 'genre1' },
                 { name: 'genre2' },
-            ])
+            ]);
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
-            expect(res.body.length).toBe(2)
-        })
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+        });
+
+        it('should handle a timer correctly', (done) => {
+            const timer = setTimeout(() => {
+                // Some code that needs to run after a delay
+                done();
+            }, 1000);
+            timer.unref();
     })
 })
 
