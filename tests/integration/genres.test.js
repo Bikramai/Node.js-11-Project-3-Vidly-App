3e9f9fbdcddc8a01477603d5e6abade9d6f508cc
bikramia <bikram.rhce@gmail.com>
2024-06-30 10:03:20 -0400

    Testing the Routes with Parameters -Need to fix this in future

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -29,6 +29,19 @@ describe('/api/genres', () => {
                 done();
             }, 1000);
             timer.unref();
+        })
+    })
+});
+
+describe('GET /:id', () => {
+    it('should return a genre if valid id is passed', async () => {
+        const genre = new Genre({ name: 'genre1' });
+        await genre.save();
+
+        const res = await request(server).get('/api/genres/' + genre._id);
+
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty(genre);
     })
 })
 
