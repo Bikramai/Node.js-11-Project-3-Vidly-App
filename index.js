23698651fa00695dd11dbe34d5398ef66e0f1bd3
bikramia <bikram.rhce@gmail.com>
2024-06-30 07:27:09 -0400

    My first Integration Test - Fail I need to fix this issue in future

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const winston = require('winston');
+// const winston = require('winston');
+// const express = require('express');
+// const app = express();
+
+// require('./startup/logging')();
+// require('./startup/routes')(app);
+// require('./startup/db')();
+// require('./startup/config')();
+// require('./startup/validation')();
+
+// // PORT
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+// module.exports = server;
+
+
 const express = require('express');
+const winston = require('winston');
 const app = express();
 
 require('./startup/logging')();
@@ -8,6 +25,17 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
-// PORT
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+process.on('uncaughtException', (ex) => {
+    winston.error('We got an uncaught exception: ', ex);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (ex) => {
+    winston.error('We got an unhandled rejection: ', ex);
+    process.exit(1);
+});
+
+module.exports = server;
